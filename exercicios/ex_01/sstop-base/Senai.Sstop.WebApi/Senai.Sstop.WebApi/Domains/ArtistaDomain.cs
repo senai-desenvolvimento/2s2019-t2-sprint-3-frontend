a96161dfe02b304c03c00f13a83ebb2bba4920c1
Helena Strada <h.strada@hotmail.com>
2019-09-20 10:31:06 -0300

    up

---
+++ exercicios/ex_01/sstop-base/Senai.Sstop.WebApi/Senai.Sstop.WebApi/Domains/ArtistaDomain.cs
@@ -0,0 +1,11 @@
+﻿namespace Senai.Sstop.WebApi.Domains
+{
+    public class ArtistaDomain
+    {
+        public int IdArtista { get; set; }
+        public string Nome { get; set; }
+        // adicionar para cadastrar
+        public int EstiloId { get; set; }
+        public EstiloDomain Estilo { get; set; }
+    }
+}
